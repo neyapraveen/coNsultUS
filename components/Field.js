dc3d44a330d445b8063d53bfbe77d8ee3a85129a
neyapraveen <74648196+neyapraveen@users.noreply.github.com>
2023-05-29 00:32:50 +0800

    Update Field.js

--- components/Field.js
+++ components/Field.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { TextInput } from "react-native";
-import { black, purple } from "../components/Constants.js";
+import React from 'react';
+import { TextInput } from 'react-native';
+import { purple, black } from './Constants';
 
 const Field = (props) => {
   return (
@@ -10,13 +10,13 @@ const Field = (props) => {
         borderRadius: 100,
         color: purple,
         paddingHorizontal: 10,
-        width: "78%",
-        height: 50,
-        backgroundColor: "rgb(220,220, 220)",
+        width: '78%',
+        height: 50, 
+        backgroundColor: 'rgb(220, 220, 220)',
         marginVertical: 10,
       }}
       placeholderTextColor='grey'
-    ></TextInput>
+    />
   );
 };
 
