e3b5654f463aface930cb33400f2e55bab158c84
neyapraveen <74648196+neyapraveen@users.noreply.github.com>
2023-06-24 22:40:10 +0800

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,99 +1,102 @@
-import React from "react";
-import { View, Text, Touchable, TouchableOpacity } from "react-native";
-import Background from "../components/Background";
-import Button from "../components/Button";
-import { black, purple } from "../components/Constants";
-import Field from "../components/Field";
+import React, { useState } from 'react';
+import {View, Text, Touchable, TouchableOpacity, SafeAreaView} from 'react-native';
+import Background from '../components/Background';
+import Button from '../components/Button';
+import {black, purple, yellow} from '../components/Constants';
+import Field from '../components/Field';
 import OfficeHoursScreen from "./Staff/OfficeHoursScreen";
 
 const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleLoginStudent = () => {
+    if (email.endsWith('@u.nus.edu')) {
+      // Email ends with "@u.nus.edu", proceed with login
+      // You can perform the login logic here
+      props.navigation.navigate('Tabs');
+      alert('Logged In as Student');
+    } else {
+      // Email does not end with "@u.nus.edu", show error message
+      alert('Invalid email format. Please enter a valid @u.nus.edu email.');
+    }
+  };
+
+  const handleLoginTeacher = () => {
+    if (email.endsWith('@u.nus.edu')) {
+      // Email ends with "@u.nus.edu", proceed with login
+      // You can perform the login logic here
+      props.navigation.navigate('OfficeHours');
+      alert('Logged In as Teacher');
+    } else {
+      // Email does not end with "@u.nus.edu", show error message
+      alert('Invalid email format. Please enter a valid @u.nus.edu email.');
+    }
+  };
+
   return (
     <Background>
-      <View style={{ alignItems: "center", width: 460 }}>
+      <SafeAreaView style={{alignItems: 'center', width: 460, }}>
         <Text
           style={{
-            color: "white",
+            color: 'white',
             fontSize: 64,
-            fontWeight: "bold",
+            fontWeight: 'bold',
             marginVertical: 20,
-          }}
-        >
+            textAlign: 'center',
+          }}>
           Login
         </Text>
         <View
           style={{
-            backgroundColor: "white",
+            backgroundColor: yellow,
             height: 700,
             width: 460,
             borderTopLeftRadius: 130,
             paddingTop: 100,
-            alignItems: "center",
-          }}
-        >
-          <Text style={{ fontSize: 40, color: black, fontWeight: "bold" }}>
+            alignItems: 'center',
+          }}>
+          <Text style={{fontSize: 40, color: black, fontWeight: 'bold', textAlign: 'center'}}>
             Welcome Back
           </Text>
           <Text
             style={{
-              color: "grey",
+              color: 'grey',
               fontSize: 19,
-              fontWeight: "bold",
+              fontWeight: 'bold',
               marginBottom: 20,
-            }}
-          >
+              textAlign: 'center',
+            }}>
             Login to your account
           </Text>
           <Field
-            placeholder="Email / Username"
-            keyboardType={"email-address"}
+            placeholder="NUSNET Email"
+            keyboardType={'email-address'}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
           />
           <Field placeholder="Password" secureTextEntry={true} />
           <View
-            style={{
-              alignItems: "flex-end",
-              width: "78%",
-              paddingRight: 16,
-              marginBottom: 200,
-            }}
-          >
-            <Text style={{ color: purple, fontWeight: "bold", fontSize: 16 }}>
-              Forgot Password?
-            </Text>
-          </View>
-          <Button
-            textColor="white"
-            bgColor={purple}
-            btnLabel="Login as student"
-            Press={() => props.navigation.navigate("Tabs")}
-          />
-          <Button
-            textColor="white"
-            bgColor={purple}
-            btnLabel="Login as teacher"
-            Press={() => props.navigation.navigate("OfficeHours")}
-          />
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
-            <Text style={{ fontSize: 16, fontWeight: "bold" }}>
-              Don't have an account ?{" "}
-            </Text>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate("Signup")}
-            >
-              <Text style={{ color: purple, fontWeight: "bold", fontSize: 16 }}>
-                Signup
+            style={{alignItems: 'flex-end', width: '78%', paddingRight: 16, marginBottom: 200}}>
+            <TouchableOpacity onPress={() => props.navigation.navigate("ResetPw")}>
+              <Text style={{color: purple, fontWeight: 'bold', fontSize: 16}}>
+                Forgot Password ?
               </Text>
             </TouchableOpacity>
           </View>
+          <View style={{ marginTop: -75}}></View>
+          <Button textColor='white' bgColor={purple} btnLabel="Login as student" Press={handleLoginStudent} />
+          <Button textColor='white' bgColor={purple} btnLabel="Login as teacher" Press={handleLoginTeacher} />
+          <View style={{ display: 'flex', flexDirection :'row', justifyContent: "center" }}>
+            <Text style={{ fontSize: 16, fontWeight:"bold" }}>Don't have an account ? </Text>
+            <TouchableOpacity onPress={() => props.navigation.navigate("Signup")}>
+            <Text style={{ color: purple, fontWeight: 'bold', fontSize: 16 }}>Signup</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      </SafeAreaView>
     </Background>
   );
 };
 
 export default Login;
+
